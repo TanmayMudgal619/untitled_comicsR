050decfdb63ac9ad425bb89c051602a6674e7c77
Riktam-Santra <32616925+Riktam-Santra@users.noreply.github.com>
2022-02-12 23:59:34 +0530

    Minor adjustments and animation addition

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -46,19 +46,22 @@ class _LoginState extends State<Login> {
                 children: [
                   Image.asset(
                     "assets/images/logo.png",
-                    width: 50,
+                    width: 100,
                   ),
                   const SizedBox(
                     height: 20,
                   ),
                   Padding(
                     padding: const EdgeInsets.only(left: 1.0),
-                    child: TextFormField(
-                      controller: usrnm,
-                      decoration: InputDecoration(
-                        labelText: "Username",
-                        labelStyle: Theme.of(context).textTheme.bodyText1,
-                        filled: true,
+                    child: SizedBox(
+                      width: 250,
+                      child: TextFormField(
+                        controller: usrnm,
+                        decoration: InputDecoration(
+                          labelText: "Username",
+                          labelStyle: Theme.of(context).textTheme.bodyText1,
+                          filled: true,
+                        ),
                       ),
                     ),
                   ),
@@ -67,107 +70,119 @@ class _LoginState extends State<Login> {
                   ),
                   Padding(
                     padding: const EdgeInsets.only(left: 1.0),
-                    child: TextFormField(
-                      controller: pswd,
-                      obscureText: !show,
-                      cursorColor: Colors.white,
-                      decoration: InputDecoration(
-                        labelStyle: Theme.of(context).textTheme.bodyText1,
-                        filled: true,
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              show = !show;
-                            });
-                          },
-                          child: Icon(
-                            (show)
-                                ? (Icons.remove_red_eye)
-                                : (Icons.remove_red_eye_outlined),
-                            size: 20,
+                    child: SizedBox(
+                      width: 250,
+                      child: TextFormField(
+                        controller: pswd,
+                        obscureText: !show,
+                        cursorColor: Colors.white,
+                        decoration: InputDecoration(
+                          labelStyle: Theme.of(context).textTheme.bodyText1,
+                          filled: true,
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                show = !show;
+                              });
+                            },
+                            child: Icon(
+                              (show)
+                                  ? (Icons.remove_red_eye)
+                                  : (Icons.remove_red_eye_outlined),
+                              size: 20,
+                            ),
                           ),
+                          labelText: 'Password',
                         ),
-                        labelText: 'Password',
                       ),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(top: 20.0),
-                    child: ListTile(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
+                    child: SizedBox(
+                      width: 165,
+                      child: ListTile(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
                         ),
-                      ),
-                      leading: const Icon(Icons.login),
-                      onTap: () async {
-                        if (usrnm.text.isNotEmpty && pswd.text.isNotEmpty) {
-                          var a;
-                          try {
-                            a = await loginUser(usrnm.text, pswd.text);
-                          } catch (e) {
-                            if (e.toString() == "Exception: Error code : 400") {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                    "Invalid Username/Password!",
+                        leading: const Icon(Icons.login),
+                        onTap: () async {
+                          if (usrnm.text.isNotEmpty && pswd.text.isNotEmpty) {
+                            var a;
+                            try {
+                              a = await loginUser(usrnm.text, pswd.text);
+                            } catch (e) {
+                              if (e.toString() ==
+                                  "Exception: Error code : 400") {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text(
+                                      "Invalid Username/Password!",
+                                    ),
                                   ),
-                                ),
-                              );
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text(e.toString()),
-                                ),
-                              );
+                                );
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    content: Text(e.toString()),
+                                  ),
+                                );
+                              }
+                              return;
                             }
-                            return;
+                            sharedPreferences.setBool("incognitoMode", false);
+                            sharedPreferences.setString(
+                                "session", a["session"]);
+                            sharedPreferences.setString(
+                                "refresh", a["refresh"]);
+                            sharedPreferences.setString("username", usrnm.text);
+                            sharedPreferences.setBool("login", true);
+                            incognitoMode = false;
+                            login = true;
+                            get();
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Loading(),
+                              ),
+                            );
                           }
-                          sharedPreferences.setBool("incognitoMode", false);
-                          sharedPreferences.setString("session", a["session"]);
-                          sharedPreferences.setString("refresh", a["refresh"]);
-                          sharedPreferences.setString("username", usrnm.text);
-                          sharedPreferences.setBool("login", true);
-                          incognitoMode = false;
-                          login = true;
-                          get();
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => Loading(),
-                            ),
-                          );
-                        }
-                      },
-                      title: const Text(
-                        "LogIn",
+                        },
+                        title: const Text(
+                          "LogIn",
+                        ),
                       ),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(top: 15.0),
-                    child: ListTile(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(15),
-                        ),
-                      ),
-                      leading: const Icon(Icons.person_off_rounded),
-                      onTap: () {
-                        sharedPreferences.clear();
-                        set();
-                        get();
-                        sharedPreferences.setBool("incognitoMode", true);
-                        incognitoMode = true;
-                        Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const Loading(),
+                    child: SizedBox(
+                      width: 155,
+                      child: ListTile(
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
                           ),
-                        );
-                      },
-                      title: const Text(
-                        "Incognito",
+                        ),
+                        leading: const Icon(Icons.person_off_rounded),
+                        onTap: () {
+                          sharedPreferences.clear();
+                          set();
+                          get();
+                          sharedPreferences.setBool("incognitoMode", true);
+                          incognitoMode = true;
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const Loading(),
+                            ),
+                          );
+                        },
+                        title: const Text(
+                          "Incognito",
+                        ),
                       ),
                     ),
                   )
